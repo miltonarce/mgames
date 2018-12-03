018f89da4396dd8c791e21aec76bc6a447efc205
Milton <miltonhernan.arce@gmail.com>
2018-12-02 22:48:58 -0300

    Se agrego funcion eliminar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
   recargarItems();
 });
 
@@ -6,11 +6,11 @@ function recargarItems() {
   ajax({
     method: "GET",
     url: "api/productos.php",
-    successCallback: function(rta) {
+    successCallback: rta => {
       let productos = JSON.parse(rta);
       let salida = "";
 
-      productos.forEach(function(item) {
+      productos.forEach(item => {
         salida += "<tr>";
         salida += "<td>" + item.idproducto + "</td>";
         salida +=
@@ -31,12 +31,30 @@ function recargarItems() {
         salida += "<td>" + item.fkidcat + "</td>";
         salida += "<td>" + item.fkidtipo + "</td>";
         salida +=
-          "<td><a href='ver-pelicula.php?id=' data-id='#'><i class='material-icons'>border_color</i></a></td>";
+          "<td><a href='#' data-id='#'><i class='material-icons'>border_color</i></a></td>";
         salida +=
-          "<td><a href='ver-pelicula.php?id=' data-id='#'><i class='material-icons'>delete_forever</i></a></td>";
+          "<td><a href='#' data-id-remove='" + item.idproducto + "'><i class='material-icons'>delete_forever</i></a></td>";
         salida += "</tr>";
       });
       document.getElementById("items").innerHTML = salida;
+      removeEventListener();
     }
+  });
+}
+
+function removeEventListener() {
+  elements = document.querySelectorAll('a[data-id-remove]');
+  elements.forEach(e => {
+    e.addEventListener("click", (el) => {
+      ajax({
+        method: "DELETE",
+        url: "api/productos.php?id=" + el.path[1].attributes[1].value,
+        successCallback: rta => {
+          let response = JSON.parse(rta);
+          console.log(response);
+        }
+      });
+      console.log(el.path[1].attributes[1].value);
+    });
   });
 }
